0d5b6cb7e71b7d5eb19b69777ca91cb623af6aa3
amemilars8 <jongiraldocad@gmail.com>
2022-03-25 18:31:08 -0500

    Pagina Inicial

---
+++ MiTiendaOnline.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MiTiendaOnline.Web.Models;
+
+namespace MiTiendaOnline.Web.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>()
+                .HasIndex(t=>t.Name)
+                .IsUnique();
+        }
+
+    }
+}
---
+++ MiTiendaOnline.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MiTiendaOnline.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
